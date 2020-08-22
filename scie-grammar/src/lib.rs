d85f7ba546759bde10fe15a5bd56d966c93c8ce3
Phodal Huang <h@phodal.com>
2020-08-22 22:52:19 +0800

    feat: import onig

--- scie-grammar/src/lib.rs
+++ scie-grammar/src/lib.rs
@@ -1,5 +1,23 @@
-use crate::inter::IRawGrammar;
+extern crate onig;
 
 pub mod inter;
 pub mod grammar;
 pub mod grammar_registry;
+
+#[cfg(test)]
+mod tests {
+    use onig::*;
+
+    #[test]
+    fn should_run_for_onig() {
+        let regex = Regex::new("e(l+)").unwrap();
+        for (i, pos) in regex.captures("hello").unwrap().iter_pos().enumerate() {
+            match pos {
+                Some((beg, end)) =>
+                    println!("Group {} captured in position {}:{}", i, beg, end),
+                None =>
+                    println!("Group {} is not captured", i)
+            }
+        }
+    }
+}
