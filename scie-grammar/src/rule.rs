27efa3db8c52b9f7dd61d9ba38a28e94b056305a
Phodal Huang <h@phodal.com>
2020-08-23 15:33:19 +0800

    feat: try to set rule

--- scie-grammar/src/rule.rs
+++ scie-grammar/src/rule.rs
@@ -1,4 +1,4 @@
-use crate::inter::IRawRepository;
+use crate::inter::{IRawRepository, IRawGrammar};
 
 pub struct RuleFactory {}
 
@@ -8,4 +8,15 @@ impl RuleFactory {
     }
 
     pub fn create_capture_rule() {}
+}
+
+// todo: trait with types
+// https://users.rust-lang.org/t/impl-trait-with-generic-function-for-generic-struct/27083/2
+pub trait IRuleRegistry {
+    fn get_rule(&self, pattern_id: number) -> Rule;
+    // fn register_rule(&self);
+}
+
+pub trait IGrammarRegistry {
+    fn get_external_grammar(&self, scope_name: String, repository: IRawRepository) -> Option<IRawGrammar>;
 }
