78c3a9ef9b86e0e5f344d2220ccda27845080e88
Phodal Huang <h@phodal.com>
2020-08-28 07:38:29 +0800

    refactor: remove unused code

--- scie-grammar/src/lib.rs
+++ scie-grammar/src/lib.rs
@@ -11,20 +11,3 @@ pub mod grammar_registry;
 pub mod inter;
 pub mod reg_exp_source;
 pub mod rule;
-pub mod support;
-
-#[cfg(test)]
-mod tests {
-    use onig::*;
-
-    #[test]
-    fn should_run_for_onig() {
-        let regex = Regex::new("e(l+)").unwrap();
-        for (i, pos) in regex.captures("hello").unwrap().iter_pos().enumerate() {
-            match pos {
-                Some((beg, end)) => println!("Group {} captured in position {}:{}", i, beg, end),
-                None => println!("Group {} is not captured", i),
-            }
-        }
-    }
-}
--- scie-grammar/src/support/mod.rs
+++
@@ -1,5 +0,0 @@
-use std::any::{Any, TypeId};
-
-pub fn get_type_of<T: ?Sized + Any>(_s: &T) -> TypeId {
-    TypeId::of::<T>()
-}
