a614fb51d37f10dc7f43bd39cfd553af28546903
Phodal Huang <h@phodal.com>
2020-09-02 20:43:44 +0800

    feat: add test case for has captures regex

--- scie-grammar/src/support/regex_source.rs
+++ scie-grammar/src/support/regex_source.rs
@@ -5,7 +5,7 @@ pub struct RegexSource {}
 
 impl RegexSource {
     pub fn has_captures(regex_source: Option<String>) -> bool {
-        let capturing_regex_source = r"/\$(\d+)|\${(\d+):\/(downcase|upcase)}/";
+        let capturing_regex_source = r"\$(\d+)|\$\{(\d+):/(downcase|upcase)\}";
 
         match regex_source {
             None => {
@@ -19,3 +19,20 @@ impl RegexSource {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use crate::support::regex_source::RegexSource;
+
+    #[test]
+    fn should_return_true_when_has_captures() {
+        let captures = RegexSource::has_captures(Some(String::from("support.function.$1.makefile")));
+        assert!(captures);
+    }
+
+    #[test]
+    fn should_return_true_when_has_downcase() {
+        let captures = RegexSource::has_captures(Some(String::from("storage.type.class.${1:/downcase}")));
+        assert!(captures);
+    }
+}
