9a60f04436e11c6405095a53604f46af3d40a496
Phodal Huang <h@phodal.com>
2020-09-02 19:42:01 +0800

    feat: add generate scope logic

--- scie-grammar/src/grammar/scope_list_element.rs
+++ scie-grammar/src/grammar/scope_list_element.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::collections::hash_map::RandomState;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ScopeListElement {
     pub parent: Option<Box<ScopeListElement>>,
@@ -14,12 +17,25 @@ impl ScopeListElement {
         }
     }
 
-    pub fn generate_scopes(&self) -> Vec<String> {
-        let result: Vec<String> = vec![];
-        let result_len = 0;
+    pub fn generate_scopes(&self) -> HashMap<i32, String> {
+        let mut result = HashMap::new();
+        let mut resultLen = 0;
 
+        let mut scope_list = self.clone();
+        let mut is_scope_list_none = false;
+        while !is_scope_list_none  {
+            result.insert(resultLen, scope_list.scope.clone());
+            match scope_list.parent {
+                None => {
+                    is_scope_list_none = true
+                },
+                Some(scope_value) => {
+                    scope_list = *scope_value.clone();
+                },
+            }
+        }
 
-        return result
+        return result.clone();
     }
 }
 
