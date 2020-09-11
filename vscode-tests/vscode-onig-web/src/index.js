89814935940937b196e53f4ebd00e200a479bffe
Phodal Huang <h@phodal.com>
2020-09-11 08:53:37 +0800

    build: setup web projects for testing

---
+++ vscode-tests/vscode-onig-web/src/index.js
@@ -0,0 +1,16 @@
+// Test import of a JavaScript function, an SVG, and Sass
+import { HelloWorld } from './js/HelloWorld'
+import WebpackLogo from './images/webpack-logo.svg'
+import './styles/index.scss'
+
+// Create SVG logo node
+const logo = document.createElement('img')
+logo.src = WebpackLogo
+
+// Create heading node
+const greeting = document.createElement('h1')
+greeting.textContent = HelloWorld()
+
+// Append SVG and heading nodes to the DOM
+const app = document.querySelector('#root')
+app.append(logo, greeting)
