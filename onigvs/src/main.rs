f8b24e9dd20737c4df5680f977230dc87d25e1d2
Phodal Huang <h@phodal.com>
2020-09-12 09:53:10 +0800

    feat: call from rust works

--- onigvs/src/main.rs
+++ onigvs/src/main.rs
@@ -1,8 +1,13 @@
+use crate::ffi::{onig_version, onig_copyright};
+use libc::{c_char, puts, strlen};
+use core::slice;
+
 mod ffi;
 
 fn main() {
-    println!("Hello, world from Rust!");
     unsafe {
-
+        let c_s = onig_copyright();
+        let str = String::from_utf8_unchecked(Vec::from(slice::from_raw_parts(c_s as *const u8, (strlen(c_s) + 1))));
+        println!("{:?}", str);
     };
 }
