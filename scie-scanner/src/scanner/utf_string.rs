44c4acf2f0bcae34268e03a577453c3bd7f991d7
Phodal Huang <h@phodal.com>
2020-09-15 07:30:10 +0800

    feat: add normal test for malloc

--- scie-scanner/src/scanner/utf_string.rs
+++ scie-scanner/src/scanner/utf_string.rs
@@ -174,6 +174,27 @@ mod tests {
         }
     }
 
+    struct NormalString {
+        pub length: i32,
+        pub str: String
+    }
+
+    impl NormalString {
+        pub fn new(str: String) -> Self {
+            NormalString { length: str.clone().len() as i32, str }
+        }
+    }
+
+    #[test]
+    fn should_create_string_with_struct_success() {
+        let long_str = "\\b(AbsoluteTime|Boolean|Byte|ByteCount|ByteOffset|BytePtr|CompTimeValue|ConstLogicalAddress|ConstStrFileNameParam|ConstStringPtr|Duration|Fixed|FixedPtr|Float32|Float32Point|Float64|Float80|Float96|FourCharCode|Fract|FractPtr|Handle|ItemCount|LogicalAddress|OptionBits|OSErr|OSStatus|OSType|OSTypePtr|PhysicalAddress|ProcessSerialNumber|ProcessSerialNumberPtr|ProcHandle|Ptr|ResType|ResTypePtr|ShortFixed|ShortFixedPtr|SignedByte|SInt16|SInt32|SInt64|SInt8|Size|StrFileName|StringHandle|StringPtr|TimeBase|TimeRecord|TimeScale|TimeValue|TimeValue64|UInt16|UInt32|UInt64|UInt8|UniChar|UniCharCount|UniCharCountPtr|UniCharPtr|UnicodeScalarValue|UniversalProcHandle|UniversalProcPtr|UnsignedFixed|UnsignedFixedPtr|UnsignedWide|UTF16Char|UTF32Char|UTF8Char)\\b";
+        let mut utf_string = NormalString::new(String::from(long_str));
+        unsafe {
+            let x = malloc(utf_string.length as u64) as *mut u32;
+            assert_ne!(*x, 0);
+        }
+    }
+
     #[test]
     fn should_convert_string_list() {
         let lists = vec![
