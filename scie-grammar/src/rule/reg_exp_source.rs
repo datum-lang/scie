dd70b5c5956f87bb1aeadef1c93128fdfd8cf995
Phodal Huang <h@phodal.com>
2020-09-29 20:24:19 +0800

    feat: add anchor cache

--- scie-grammar/src/rule/reg_exp_source.rs
+++ scie-grammar/src/rule/reg_exp_source.rs
@@ -3,19 +3,19 @@ use crate::rule::CompiledRule;
 
 #[derive(Clone, Debug, Serialize)]
 pub struct AnchorCache {
-    a0_g0: Option<String>,
-    a0_g1: Option<String>,
-    a1_g0: Option<String>,
-    a1_g1: Option<String>,
+    a0_g0: String,
+    a0_g1: String,
+    a1_g0: String,
+    a1_g1: String,
 }
 
 impl Default for AnchorCache {
     fn default() -> Self {
         AnchorCache {
-            a0_g0: None,
-            a0_g1: None,
-            a1_g0: None,
-            a1_g1: None,
+            a0_g0: String::from(""),
+            a0_g1: String::from(""),
+            a1_g0: String::from(""),
+            a1_g1: String::from(""),
         }
     }
 }
@@ -90,6 +90,7 @@ pub struct RegExpSource {
     pub source: String,
     pub rule_id: i32,
     pub has_anchor: bool,
+    pub _anchor_cache: Option<Box<AnchorCache>>,
 }
 
 impl RegExpSource {
@@ -122,6 +123,8 @@ impl RegExpSource {
             pos = pos + 1;
         }
 
+        let anchor_cache: Option<Box<AnchorCache>> = None;
+
         if last_pushed_pos == 0 {
             result = exp_source.clone()
         } else {
@@ -129,11 +132,75 @@ impl RegExpSource {
             result = output.join("");
         }
 
-        RegExpSource {
+        let mut reg_exp_source = RegExpSource {
             source: result,
             rule_id,
             has_anchor,
+            _anchor_cache: anchor_cache,
+        };
+
+        let cache = reg_exp_source.build_cache();
+        reg_exp_source._anchor_cache = Some(Box::from(cache));
+
+        reg_exp_source
+    }
+    fn build_cache(&self) -> AnchorCache {
+        let length = self.source.len();
+
+        let mut a0_g0_result: Vec<String> = vec![];
+        let mut a0_g1_result: Vec<String> = vec![];
+        let mut a1_g0_result: Vec<String> = vec![];
+        let mut a1_g1_result: Vec<String> = vec![];
+
+        a0_g0_result.resize(length, String::from(""));
+        a0_g1_result.resize(length, String::from(""));
+        a1_g0_result.resize(length, String::from(""));
+        a1_g1_result.resize(length, String::from(""));
+
+        let mut pos = 0;
+        let mut ch: char;
+        let mut next_char: char;
+
+        while pos < length {
+            ch = self.source.clone().chars().nth(pos).unwrap();
+            a0_g0_result[pos] = ch.to_string();
+            a0_g1_result[pos] = ch.to_string();
+            a1_g0_result[pos] = ch.to_string();
+            a1_g1_result[pos] = ch.to_string();
+
+            if ch == '\\' {
+                if pos + 1 < length {
+                    next_char = self.source.clone().chars().nth(pos + 1).unwrap();
+                    if next_char == 'A' {
+                        a0_g0_result[pos + 1] = String::from("\u{FFFF}");
+                        a0_g1_result[pos + 1] = String::from("\u{FFFF}");
+                        a1_g0_result[pos + 1] = String::from("A");
+                        a1_g1_result[pos + 1] = String::from("A");
+                    } else if next_char == 'G' {
+                        a0_g0_result[pos + 1] = String::from("\u{FFFF}");
+                        a0_g1_result[pos + 1] = String::from("G");
+                        a1_g0_result[pos + 1] = String::from("\u{FFFF}");
+                        a1_g1_result[pos + 1] = String::from("G");
+                    } else {
+                        a0_g0_result[pos] = String::from(next_char.clone());
+                        a0_g1_result[pos] = String::from(next_char.clone());
+                        a1_g0_result[pos] = String::from(next_char.clone());
+                        a1_g1_result[pos] = String::from(next_char.clone());
+                    }
+
+                    pos = pos + 1;
+                }
+            }
+
+            pos = pos + 1;
         }
+
+        return AnchorCache {
+            a0_g0: a0_g0_result.join(""),
+            a0_g1: a0_g1_result.join(""),
+            a1_g0: a1_g0_result.join(""),
+            a1_g1: a1_g1_result.join(""),
+        };
     }
 }
 
@@ -152,6 +219,16 @@ mod tests {
     fn should_change_resource_for_z() {
         let source = RegExpSource::new(String::from("\\z"), 1);
         assert_eq!("$(?!\n)(?<!\n)", source.source);
+    }
+
+    #[test]
+    fn should_build_anchor_cache_for_g() {
+        let source = RegExpSource::new(String::from("\\G"), 1);
+        let cache = source._anchor_cache.unwrap();
+        assert_eq!("\\\u{ffff}", cache.a0_g0);
+        assert_eq!("\\G", cache.a0_g1);
+        assert_eq!("\\\u{ffff}", cache.a1_g0);
+        assert_eq!("\\G", cache.a1_g1);
     }
 }
 
