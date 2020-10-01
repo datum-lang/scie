0ad18d982f36af02387bf7b2c8607b2068272cb6
Phodal Huang <h@phodal.com>
2020-10-01 21:53:13 +0800

    feat: align output for compare results

--- scie-grammar/src/grammar/grammar.rs
+++ scie-grammar/src/grammar/grammar.rs
@@ -685,7 +685,7 @@ pub fn to_grammar_with_code(grammar_path: &str, code: &str) -> Grammar {
                 .collect();
             let token_str: String = token.scopes.join(", ");
             println!(
-                " - token from {:?} to {:?} ({:?}) with scopes {:?}",
+                " - token from {} to {} ({}) with scopes {}",
                 token.start_index, token.end_index, new_line, token_str
             )
         }
