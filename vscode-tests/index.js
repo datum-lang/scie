fd2b1c20a28175f344933dc27c8c33428815636e
Phodal Huang <h@phodal.com>
2020-10-08 21:38:11 +0800

    feat: add test for ms match

--- vscode-tests/index.js
+++ vscode-tests/index.js
@@ -21,26 +21,32 @@ const registry = new vsctm.Registry({
         createOnigString: (str) => new oniguruma.OnigString(str)
     }),
     loadGrammar: (scopeName) => {
-        return readFile('./syntaxes/json/json.json').then(data => vsctm.parseRawGrammar(data.toString(), "c.json"))
+        return readFile('./syntaxes/json/javascript.json').then(data => vsctm.parseRawGrammar(data.toString(), "c.json"))
     }
 });
 
-registry.loadGrammar('source.json').then(grammar => {
-    var content = fs.readFileSync("../benchmark/samples/JavaScript.tmLanguage.json.txt");
+registry.loadGrammar('source.js').then(grammar => {
+    var content = fs.readFileSync("../benchmark/samples/large.js.txt");
     var text = content.toString().split("\n");
     let ruleStack = vsctm.INITIAL;
+
+    const start = Date.now();
+
     for (let i = 0; i < text.length; i++) {
         const line = text[i];
         const lineTokens = grammar.tokenizeLine(line, ruleStack);
-        for (let j = 0; j < lineTokens.tokens.length; j++) {
-            const token = lineTokens.tokens[j];
-            console.log(` - token from ${token.startIndex} to ${token.endIndex} ` +
-                `(${line.substring(token.startIndex, token.endIndex)}) ` +
-                `with scopes ${token.scopes.join(', ')}`
-            );
-        }
+        // for (let j = 0; j < lineTokens.tokens.length; j++) {
+        //     const token = lineTokens.tokens[j];
+        //     console.log(` - token from ${token.startIndex} to ${token.endIndex} ` +
+        //         `(${line.substring(token.startIndex, token.endIndex)}) ` +
+        //         `with scopes ${token.scopes.join(', ')}`
+        //     );
+        // }
         ruleStack = lineTokens.ruleStack;
     }
+
+    const millis = Date.now() - start;
+    console.log(`seconds elapsed = ${Math.floor(millis)}`);
 });
 //
 // console.log("____________________________");
