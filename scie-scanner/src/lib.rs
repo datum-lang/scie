99212b72852200d15f29ac7f12322e64ba62d79d
Phodal Huang <h@phodal.com>
2020-10-09 21:21:02 +0800

    feat: change rust malloc to jemalloc for resolve crash issue

--- scie-scanner/src/lib.rs
+++ scie-scanner/src/lib.rs
@@ -1,3 +1,9 @@
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 #[macro_use]
 extern crate serde_derive;
 
