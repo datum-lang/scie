31ac741d8aabd6f6ddde4494b050f28b45abe30a
Phodal Huang <h@phodal.com>
2020-10-10 12:36:53 +0800

    feat: add exlude patterns from vcs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[macro_use]
+extern crate lazy_static;
+
 pub mod artifact;
 pub mod bindata;
 
-fn main() {}
+lazy_static! {
+  static ref DEFAULT_VCS_EXCLUDES: Vec<&'static str> = vec![
+        "**/%*%",
+        "**/.#*",
+        "**/._*",
+        "**/#*#",
+        "**/*~",
+        "**/.DS_Store",
+
+        "**/CVS",
+        "**/CVS/**",
+        "**/.cvsignore",
+
+        "**/SCCS",
+        "**/SCCS/**",
+
+        "**/.bzr",
+        "**/.bzr/**",
+        "**/.bzrignore",
+
+        "**/vssver.scc",
+
+        "**/.hg",
+        "**/.hg/**",
+        "**/.hgtags",
+        "**/.hgignore",
+        "**/.hgsubstate",
+        "**/.hgsub",
+
+        "**/.svn",
+        "**/.svn/**",
+
+        "**/.git",
+        "**/.git/**",
+        "**/.gitignore",
+        "**/.gitmodules",
+        "**/.gitattributes"
+    ];
+
+}
+
+fn main() {
+}
