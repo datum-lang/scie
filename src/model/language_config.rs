e682e03320ce2271c0eecb175236fcc8fa89d984
Phodal Huang <h@phodal.com>
2020-10-10 23:04:12 +0800

    feat: try to add struct for langauge config

--- src/model/language_config.rs
+++ src/model/language_config.rs
@@ -1,5 +1,17 @@
-pub struct LanguageConfig {
+pub struct CommentRule {
+    #[serde(alias = "lineComment")]
+    pub line_comment: String,
+    #[serde(alias = "blockComment")]
+    pub block_comment: CharacterPair,
+}
 
+type CharacterPair = Vec<String>;
+
+pub struct LanguageConfig {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub comments: CommentRule,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub brackets: Vec<CharacterPair>,
 }
 
 impl LanguageConfig {
