13ff55dfbb32f114a05ace1d386d7535ae8ccb2b
Phodal Huang <h@phodal.com>
2020-10-10 13:19:07 +0800

    test: add first element for testing

--- src/artifact/mod.rs
+++ src/artifact/mod.rs
@@ -1,20 +1,20 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Workspace {
     #[serde(alias = "workspaceName")]
     pub name: String,
     pub projects: Vec<Project>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Project {
     #[serde(alias = "projectName")]
     pub name: String,
     pub modules: Vec<Module>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Module {
     #[serde(alias = "moduleName")]
     pub name: String,
@@ -22,27 +22,26 @@ pub struct Module {
     pub artifacts: Vec<Artifact>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Artifact {
     #[serde(alias = "artifactName")]
     pub name: String,
     pub out: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Package {
     #[serde(alias = "packageName")]
     pub name: String,
     pub files: Vec<CodeFile>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct CodeFile {
-    pub elements: Vec<Element>,
-    pub scopes: Vec<ElementScope>
+    pub elements: Vec<Element>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Element {
     #[serde(alias = "lineNum")]
     pub line_num: i32,
@@ -50,10 +49,11 @@ pub struct Element {
     pub start_index: i32,
     #[serde(alias = "end")]
     pub end_index: i32,
-    pub value: Vec<String>,
+    pub value: String,
+    pub scopes: Vec<ElementScope>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ElementScope {
     #[serde(alias = "scopeName")]
     pub name: String,
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ extern crate serde_derive;
 #[macro_use]
 extern crate lazy_static;
 
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
+
 pub mod artifact;
 pub mod bindata;
 
@@ -48,5 +52,62 @@ lazy_static! {
 
 }
 
-fn main() {
+fn main() {}
+
+fn read_code(lang_test_dir: &PathBuf) -> String {
+    let mut file = File::open(lang_test_dir).unwrap();
+    let mut code = String::new();
+    file.read_to_string(&mut code).unwrap();
+    code
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use scie_grammar::grammar::{Grammar, StackElement};
+    use crate::artifact::{Element};
+    use crate::read_code;
+
+    #[test]
+    fn should_build_first_file() {
+        let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).to_path_buf();
+        let lang = root_dir.clone()
+            .join("extensions").join("java").join("syntaxes").join("java.tmLanguage.json");
+
+        let code_dir = root_dir.join("fixtures").join("test-cases").join("e2e").join("java").join("HelloWorld.java");
+        let code = read_code(&code_dir);
+
+        let mut elements: Vec<Element> = vec![];
+        let mut grammar = Grammar::from_file(lang.to_str().unwrap());
+        let mut rule_stack = Some(StackElement::null());
+
+        let mut line_num = 1;
+        for line in code.lines() {
+            let result = grammar.tokenize_line(line, &mut rule_stack);
+            for token in result.tokens {
+                let start = token.start_index;
+                let end = token.end_index;
+                let text: String = String::from(line)
+                    .chars()
+                    .skip(start as usize)
+                    .take((end - start) as usize)
+                    .collect();
+
+                elements.push(
+                    Element {
+                        line_num,
+                        start_index: start,
+                        end_index: end,
+                        value: text,
+                        scopes: vec![],
+                    }
+                );
+            }
+
+            rule_stack = result.rule_stack;
+            line_num = line_num + 1
+        }
+
+        println!("{:?}", elements);
+    }
 }
