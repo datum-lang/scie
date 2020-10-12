3a63802b934e2197f8b3001b73d3ba112ff5a308
Phodal Huang <h@phodal.com>
2020-10-12 20:25:20 +0800

    feat: fix test for ./ issue

--- scie-bingen/src/grammar_gen.rs
+++ scie-bingen/src/grammar_gen.rs
@@ -21,10 +21,12 @@ impl GrammarGen {
         let langs = LangExtGen::default();
         let mut raw_grammar_map: BTreeMap<String, LangConfig> = Default::default();
         for (ext, entry) in langs.ext_map.iter() {
-            let mut grammar_path = entry.path.clone();
             let lang = entry.name.clone();
             let tm_grammar = langs.grammar_map[&lang].clone();
+
+            let mut grammar_path = entry.path.clone();
             grammar_path.push_str(&*tm_grammar.path);
+            grammar_path = grammar_path.replace("./", "/");
 
             if tm_grammar.language.is_some() {
                 raw_grammar_map.insert(
@@ -49,6 +51,6 @@ mod tests {
     #[test]
     fn should_build_default_maps() {
         let map = GrammarGen::build_grammar_bin_data();
-        println!("{:?}", map);
+        assert!(map[".zshrc"].path.contains("extensions/shellscript/syntaxes/shell-unix-bash.tmLanguage.json"));
     }
 }
