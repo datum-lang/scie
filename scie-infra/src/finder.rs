9e87af99255e2e2d941e6674efd8e76a8c95bf28
Phodal Huang <h@phodal.com>
2020-10-13 21:11:09 +0800

    feat: add gtignore rule for finder

--- scie-infra/src/finder.rs
+++ scie-infra/src/finder.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 lazy_static! {
     static ref DEFAULT_VCS_EXCLUDES: Vec<&'static str> = vec![
@@ -35,7 +36,6 @@ lazy_static! {
     ];
 }
 
-
 pub struct Finder {}
 
 impl Finder {
@@ -54,15 +54,58 @@ impl Finder {
         }
     }
 
-    pub fn get_files(file_path: &PathBuf, gitignore: Option<&PathBuf>)  {
+    pub fn get_files(dir: &PathBuf, gitignore_path: Option<&PathBuf>) -> Vec<PathBuf> {
+        let walk_dir = WalkDir::new(dir);
 
+        let mut files = vec![];
+        match gitignore_path {
+            None => {
+                for entry in walk_dir.into_iter() {
+                    if entry.is_err() {
+                        continue;
+                    };
+
+                    files.push(entry.unwrap().path().to_path_buf());
+                }
+            }
+            Some(gitignore) => {
+                let file = gitignore::File::new(&gitignore).unwrap();
+                for entry in walk_dir.into_iter() {
+                    if entry.is_err() {
+                        continue;
+                    };
+                    let path = entry.unwrap().path().to_path_buf();
+                    if !file.is_excluded(&*path).unwrap() {
+                        files.push(path);
+                    }
+                }
+            }
+        }
+
+        return files;
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::finder::Finder;
+    use std::path::PathBuf;
+
     #[test]
     fn should_filter_gitignore_rules() {
-        assert_eq!(2 + 2, 4);
+        let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .parent()
+            .unwrap()
+            .to_path_buf();
+        let code_dir = root_dir
+            .clone()
+            .join("fixtures")
+            .join("finder")
+            .join("ignore-test");
+
+        let ignore_path = code_dir.clone().join(".gitignore");
+
+        let files = Finder::get_files(&code_dir, Some(&ignore_path));
+        assert_eq!(1, files.len())
     }
 }
