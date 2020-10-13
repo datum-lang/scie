2da93ec55ace91681e027c42072ba0a7375519f4
Phodal Huang <h@phodal.com>
2020-10-13 08:56:45 +0800

    test: init framework run func test

--- scie-detector/src/framework_detector.rs
+++ scie-detector/src/framework_detector.rs
@@ -1,3 +1,5 @@
+use walkdir::WalkDir;
+
 pub struct Framework {
     pub name: String,
     pub path: String,
@@ -14,19 +16,45 @@ pub struct Framework {
 
 pub struct FrameworkDetector {
     pub names: Vec<String>,
-    pub frameworks: Vec<Framework>
+    pub frameworks: Vec<Framework>,
 }
 
 impl FrameworkDetector {
     pub fn new() -> Self {
         FrameworkDetector {
             names: vec![],
-            frameworks: vec![]
+            frameworks: vec![],
         }
     }
 
-    pub fn run() {
+    pub fn run(&self, path: String) {
+        let walk_dir = WalkDir::new(path);
+        for path in walk_dir.max_depth(1).into_iter() {
+            println!("{:?}", path);
+        }
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use crate::framework_detector::FrameworkDetector;
+
+    #[test]
+    fn it_works() {
+        let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .parent()
+            .unwrap()
+            .to_path_buf();
+
+        let test_project_dir = root_dir.clone()
+            .join("fixtures")
+            .join("projects")
+            .join("java")
+            .join("simple");
+
+        let detector = FrameworkDetector::new();
+        detector.run(test_project_dir.display().to_string());
     }
 }
 
--- scie-detector/src/lib.rs
+++ scie-detector/src/lib.rs
@@ -1,9 +1,1 @@
 pub mod framework_detector;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
