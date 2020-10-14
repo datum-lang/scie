b8edb0fea42181c7ea084326cc989667a76f63da
Phodal Huang <h@phodal.com>
2020-10-14 20:47:16 +0800

    build: init cli command

--- scie-cli/src/main.rs
+++ scie-cli/src/main.rs
@@ -1,3 +1,66 @@
+// (Full example with detailed comments in examples/01d_quick_example.rs)
+//
+// This example demonstrates clap's full 'custom derive' style of creating arguments which is the
+// simplest method of use, but sacrifices some flexibility.
+use clap::Clap;
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Kevin K. <kbknapp@gmail.com>")]
+struct Opts {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short, long, default_value = "default.conf")]
+    config: String,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: String,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(version = "1.3", author = "Someone E. <someone_else@other.com>")]
+    Test(Test),
+}
+
+/// A subcommand for controlling testing
+#[derive(Clap)]
+struct Test {
+    /// Print debug info
+    #[clap(short)]
+    debug: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    println!("Value for config: {}", opts.config);
+    println!("Using input file: {}", opts.input);
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::Test(t) => {
+            if t.debug {
+                println!("Printing debug info...");
+            } else {
+                println!("Printing normally...");
+            }
+        }
+    }
+    // more program logic goes here...
 }
