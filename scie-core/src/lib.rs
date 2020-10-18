b10a33de372bb04bb3082e5e585016fafc849932
Phodal Huang <h@phodal.com>
2020-10-18 20:31:21 +0800

    feat: try to seek files

--- scie-core/src/lib.rs
+++ scie-core/src/lib.rs
@@ -1,16 +1,42 @@
-extern crate serde_derive;
-
+#[macro_use]
 extern crate serde;
+
+extern crate serde_derive;
 
 pub mod analyser;
 pub mod identify;
 pub mod sima;
 
-
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn should_re_build_code_file() {
+    use std::fs::File;
+    use std::io::{Cursor, Read, Seek, SeekFrom, Write};
 
+    #[test]
+    fn should_build_fake_file() {
+        let mut c = Cursor::new(Vec::new());
+
+        let demo = "Demo";
+
+        c.write_all(demo.as_ref()).unwrap();
+        c.write_all("\n".as_ref()).unwrap();
+
+        c.seek(SeekFrom::Start(0)).unwrap();
+
+        output_to_file(&mut c);
+    }
+
+    fn output_to_file(c: &mut Cursor<Vec<u8>>) {
+        let mut file = File::create("foo.txt").unwrap();
+
+        let mut out = Vec::new();
+        c.read_to_end(&mut out).unwrap();
+        file.write_all(&*out).unwrap();
+    }
+
+    fn output_to_vec(c: &mut Cursor<Vec<u8>>) {
+        let mut out = Vec::new();
+        c.read_to_end(&mut out).unwrap();
+        println!("{:?}", out);
     }
 }
