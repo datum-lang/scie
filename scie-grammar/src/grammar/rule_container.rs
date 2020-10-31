621c17f0468f2a1e940e71f65dfdb9402769e532
Phodal Huang <h@phodal.com>
2020-10-31 20:36:25 +0800

    feat: create logic in rule container

--- scie-grammar/src/grammar/rule_container.rs
+++ scie-grammar/src/grammar/rule_container.rs
@@ -1,15 +1,37 @@
-use crate::rule::AbstractRule;
+use crate::rule::{AbstractRule, EmptyRule};
 use std::collections::HashMap as Map;
 
 #[derive(Debug, Clone)]
 pub struct RuleContainer {
+    _empty_rule: Map<i32, Box<dyn AbstractRule>>,
     pub rule_id2desc: Map<i32, Box<dyn AbstractRule>>,
 }
 
 impl Default for RuleContainer {
     fn default() -> Self {
-        RuleContainer {
+        let mut _empty_rule = Map::new();
+
+        let mut container = RuleContainer {
+            _empty_rule,
             rule_id2desc: Default::default(),
-        }
+        };
+
+        container._empty_rule.insert(-2, Box::new(EmptyRule {}));
+        container
+    }
+}
+
+impl RuleContainer {
+    fn get_rule(&mut self, pattern_id: i32) -> &mut Box<dyn AbstractRule> {
+        return self
+            .rule_id2desc
+            .get_mut(&pattern_id)
+            .unwrap_or(self._empty_rule.get_mut(&-2).unwrap());
+    }
+
+    fn register_rule(&mut self, result: Box<dyn AbstractRule>) -> i32 {
+        let id = result.id();
+        self.rule_id2desc.insert(id, result);
+        id
     }
 }
