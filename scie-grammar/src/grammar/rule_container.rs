0d063664a648c6a55db5c083a5f702ab564be697
Phodal Huang <h@phodal.com>
2020-12-09 19:56:39 +0800

    refactor: inline rules to rule_container

--- scie-grammar/src/grammar/rule_container.rs
+++ scie-grammar/src/grammar/rule_container.rs
@@ -1,6 +1,9 @@
 use crate::grammar::StackElement;
 use crate::rule::abstract_rule::RuleEnum;
-use crate::rule::{AbstractRule, CompiledRule, EmptyRule, RegExpSourceList};
+use crate::rule::{
+    AbstractRule, BeginEndRule, BeginWhileRule, CompiledRule, EmptyRule, IncludeOnlyRule,
+    MatchRule, RegExpSourceList,
+};
 use std::cell::RefCell;
 use std::collections::{HashMap as Map, HashMap};
 use std::rc::Rc;
@@ -47,15 +50,191 @@ impl RuleContainer {
         allow_a: bool,
         allow_g: bool,
     ) -> CompiledRule {
-        let rule_id = stack.rule_id;
-        // https://stackoverflow.com/questions/44453398/how-can-i-borrow-from-a-hashmap-to-read-and-write-at-the-same-time
-        let mut rule = self.get_rule(rule_id).clone();
-        // let mut rule = &mut self.rules[id];
-        let rule_scanner = rule.compile(&mut self.rule_id2desc, &stack.end_rule, allow_a, allow_g);
+        let (rule, rule_scanner) = RuleContainer::compile(
+            stack.rule_id,
+            &mut self.rule_id2desc,
+            &stack.end_rule,
+            allow_a,
+            allow_g,
+        );
 
         self.register_rule(rule);
 
         rule_scanner
+    }
+
+    pub fn compile(
+        rule_id: i32,
+        container: &mut HashMap<i32, Box<dyn AbstractRule>>,
+        end: &Option<String>,
+        allow_a: bool,
+        allow_g: bool,
+    ) -> (Box<dyn AbstractRule>, CompiledRule) {
+        // todo: temp for clone
+        let mut rule = container.get_mut(&rule_id).unwrap().clone();
+
+        let compiled;
+        match rule.get_rule_instance() {
+            RuleEnum::BeginEndRule(_) => {
+                let begin = rule.get_instance().downcast_mut::<BeginEndRule>().unwrap();
+                compiled =
+                    RuleContainer::compile_begin_end_rule(begin, container, end, allow_a, allow_g);
+            }
+            RuleEnum::BeginWhileRule(_) => {
+                let while_rule = rule
+                    .get_instance()
+                    .downcast_mut::<BeginWhileRule>()
+                    .unwrap();
+                compiled = RuleContainer::compile_begin_while_rule(
+                    while_rule, container, end, allow_a, allow_g,
+                );
+            }
+            RuleEnum::MatchRule(_) => {
+                let match_rule = rule.get_instance().downcast_mut::<MatchRule>().unwrap();
+                compiled =
+                    RuleContainer::compile_match_rule(match_rule, container, end, allow_a, allow_g);
+            }
+            RuleEnum::CaptureRule(_) => unimplemented!(),
+            RuleEnum::EmptyRule(_) => unimplemented!(),
+            RuleEnum::IncludeOnlyRule(_) => {
+                let include = rule
+                    .get_instance()
+                    .downcast_mut::<IncludeOnlyRule>()
+                    .unwrap();
+                compiled =
+                    RuleContainer::compile_include_only(include, container, end, allow_a, allow_g);
+            }
+        };
+
+        return (rule, compiled);
+    }
+
+    fn compile_include_only(
+        rule: &mut IncludeOnlyRule,
+        container: &mut HashMap<i32, Box<dyn AbstractRule>>,
+        _end_regex_source: &Option<String>,
+        allow_a: bool,
+        allow_g: bool,
+    ) -> CompiledRule {
+        if rule._cached_compiled_patterns.is_none() {
+            let mut cached_compiled_patterns = RegExpSourceList::new();
+            RuleContainer::collect_patterns_recursive(
+                rule.id(),
+                container,
+                &mut cached_compiled_patterns,
+                true,
+            );
+
+            // self.collect_patterns_recursive(container, &mut cached_compiled_patterns, true);
+            rule._cached_compiled_patterns = Some(cached_compiled_patterns);
+        }
+
+        return *rule
+            ._cached_compiled_patterns
+            .as_mut()
+            .unwrap()
+            .compile(allow_a, allow_g);
+    }
+
+    fn compile_match_rule(
+        rule: &mut MatchRule,
+        container: &mut HashMap<i32, Box<dyn AbstractRule>>,
+        _end_regex_source: &Option<String>,
+        allow_a: bool,
+        allow_g: bool,
+    ) -> CompiledRule {
+        if rule._cached_compiled_patterns.is_none() {
+            let mut cached_compiled_patterns = RegExpSourceList::new();
+            // self.collect_patterns_recursive(container, &mut cached_compiled_patterns, true);
+            RuleContainer::collect_patterns_recursive(
+                rule.id(),
+                container,
+                &mut cached_compiled_patterns,
+                true,
+            );
+            rule._cached_compiled_patterns = Some(cached_compiled_patterns);
+        }
+
+        return *rule
+            ._cached_compiled_patterns
+            .as_mut()
+            .unwrap()
+            .compile(allow_a, allow_g);
+    }
+
+    fn compile_begin_while_rule(
+        rule: &mut BeginWhileRule,
+        container: &mut HashMap<i32, Box<dyn AbstractRule>>,
+        _end_regex_source: &Option<String>,
+        allow_a: bool,
+        allow_g: bool,
+    ) -> CompiledRule {
+        if rule._cached_compiled_patterns.is_none() {
+            let mut cached_compiled_patterns = RegExpSourceList::new();
+
+            RuleContainer::collect_patterns_recursive(
+                rule.id(),
+                container,
+                &mut cached_compiled_patterns,
+                true,
+            );
+            rule._cached_compiled_patterns = Some(cached_compiled_patterns);
+        }
+
+        return *rule
+            ._cached_compiled_patterns
+            .as_mut()
+            .unwrap()
+            .compile(allow_a, allow_g);
+    }
+
+    pub fn compile_begin_end_rule(
+        rule: &mut BeginEndRule,
+        container: &mut HashMap<i32, Box<dyn AbstractRule>>,
+        end_regex_source: &Option<String>,
+        allow_a: bool,
+        allow_g: bool,
+    ) -> CompiledRule {
+        if rule._cached_compiled_patterns.is_none() {
+            let mut cached_compiled_patterns = RegExpSourceList::new();
+
+            RuleContainer::collect_patterns_recursive(
+                rule.id(),
+                container,
+                &mut cached_compiled_patterns,
+                true,
+            );
+
+            if rule.apply_end_pattern_last {
+                cached_compiled_patterns.push(rule._end.clone());
+            } else {
+                cached_compiled_patterns.unshift(rule._end.clone());
+            }
+
+            rule._cached_compiled_patterns = Some(cached_compiled_patterns);
+        }
+
+        if rule._end.has_back_references {
+            if rule.apply_end_pattern_last {
+                let length = rule._cached_compiled_patterns.as_ref().unwrap().length();
+
+                rule._cached_compiled_patterns
+                    .as_mut()
+                    .unwrap()
+                    .set_source(length - 1, end_regex_source.as_ref().unwrap())
+            } else {
+                rule._cached_compiled_patterns
+                    .as_mut()
+                    .unwrap()
+                    .set_source(0, end_regex_source.as_ref().unwrap())
+            }
+        }
+
+        return *rule
+            ._cached_compiled_patterns
+            .as_mut()
+            .unwrap()
+            .compile(allow_a, allow_g);
     }
 
     pub fn collect_patterns_recursive(
